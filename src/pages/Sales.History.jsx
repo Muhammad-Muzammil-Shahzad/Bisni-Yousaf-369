0c981ded4b8899fb8bb7b3a4f921f9e268c89cbb
MUHAMMAD-MUZAMMIL <muzammillrh999@gmail.com>
2026-09-10 11:34:54 -0700

    Update Sales.History.jsx

--- src/pages/Sales.History.jsx
+++ src/pages/Sales.History.jsx
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api'; // Adjust this to your backend API base URL
+const API_BASE_URL = 'https://bisni-ms-backend.onrender.com/api';
 
 const EmployeeItemsSold = () => {
   const [itemsData, setItemsData] = useState([]);
