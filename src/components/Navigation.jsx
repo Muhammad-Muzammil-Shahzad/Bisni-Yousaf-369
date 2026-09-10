e79b4105ff9e7ae36f243a67b2007ed04be6c111
MUHAMMAD-MUZAMMIL <muzammillrh999@gmail.com>
2026-09-10 11:45:26 -0700

    Update Navigation.jsx

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -70,6 +70,18 @@ const Navigation = () => {
         </svg>
       ),
     },
+    {
+      id: 'history',
+      label: 'History',
+      icon: (
+        <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4" />
+        </svg>
+      ),
+      children: [
+        { path: '/sales_history', label: 'Sales', },
+      ],
+    }
   ];
 
   const toggleDropdown = (id) => {
