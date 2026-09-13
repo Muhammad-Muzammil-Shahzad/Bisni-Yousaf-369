01c294e7a85c52d962f3b4eb6d14aedda2e880e5
MUHAMMAD-MUZAMMIL <muzammillrh999@gmail.com>
2026-09-13 02:47:48 -0700

    Update CategoryProductSales.jsx

--- src/pages/CategoryProductSales.jsx
+++ src/pages/CategoryProductSales.jsx
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://bisni-ms-backend.onrender.com/api';
 
 const CategoryProductSales = () => {
   const [invoices, setInvoices] = useState([]);
